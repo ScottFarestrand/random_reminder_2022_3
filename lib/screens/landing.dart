8378fdc83d6fa9a82cc291635ac58b0b7c976876
ScottFarestrand <scottfarestrand@gmail.com>
2022-06-11 08:18:01 -0700

    Added navigation to login, verify or register

--- lib/screens/landing.dart
+++ lib/screens/landing.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import '../widgets/Login.dart';
 import '../widgets/ValidateEmail.dart';
 import '../widgets/register.dart';
@@ -20,7 +21,8 @@ class Landing extends StatefulWidget {
 }
 
 class _LandingState extends State<Landing> {
-  var pageWidget = pageWidgets.login;
+  var pageWidget = pageWidgets.landing;
+
   void setStateLoggedIn(){
     ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
@@ -37,26 +39,66 @@ class _LandingState extends State<Landing> {
     });
   }
   void setStateRegister(){
+    print("Register");
     setState((){
-      pageWidget = pageWidgets.validate;
+      pageWidget = pageWidgets.landing;
     });
   }
   @override
   Widget build(BuildContext context) {
+    bool isVerified = false;
     if (pageWidget == pageWidgets.landing) {
-      return Login(loginCallBack: setStateLoggedIn,
-          registerCallBack: setStateRegister);
+      return Scaffold(
+          backgroundColor: Theme
+              .of(context)
+              .colorScheme
+              .primary,
+          appBar: AppBar(
+            title: Text("Random Reminder"),
+          ),
+          // body: Login(),
+          body: StreamBuilder<User?>(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                print("Yes");
+                final user = FirebaseAuth.instance.currentUser!;
+                isVerified = user.emailVerified;
+                if (!isVerified) {
+                  print("Verify");
+                  // return VerifyEmail();
+                  return Text("Timee to Verify Email");
+                }
+                // return Text('Time to Login');
+                return Login(loginCallBack: setStateLoggedIn,
+                    registerCallBack: setStateRegister);
+              } else {
+                return Login(loginCallBack: setStateLoggedIn,
+                    registerCallBack: setStateRegister);
+                // return Login();
+              }
+            },
+          )
+      );
     }
-
-    if (pageWidget == pageWidgets.validate) {
-      return validateEmail(callback: setStateValidated,);
+    else {
+      return Register(callback: setStateRegister);
     }
-    if (pageWidget == pageWidgets.register) {
-      return Register(callback:setStateValidated );
-    }
-    // if (userState == appState.validated) {}
-    return Login(loginCallBack: setStateLoggedIn,
-        registerCallBack: setStateRegister);
+    
+    // if (pageWidget == pageWidgets.landing) {
+    //   return Login(loginCallBack: setStateLoggedIn,
+    //       registerCallBack: setStateRegister);
+    // }
+    //
+    // if (pageWidget == pageWidgets.validate) {
+    //   return validateEmail(callback: setStateValidated,);
+    // }
+    // if (pageWidget == pageWidgets.register) {
+    //   return Register(callback:setStateValidated );
+    // }
+    // // if (userState == appState.validated) {}
+    // return Login(loginCallBack: setStateLoggedIn,
+    //     registerCallBack: setStateRegister);
 
   }
 
--- lib/widgets/Login.dart
+++ lib/widgets/Login.dart
@@ -15,26 +15,20 @@ class Login extends StatelessWidget {
     final passwordController = TextEditingController();
     emailController.text = "ScottFarestrand@gmail.com";
     passwordController.text = "Jlj#980507";
-    return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      // drawer: NavDrawer(),
-      appBar: AppBar(
-        title: Text("Log in/Register"),
-      ),
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(15, 8, 15, 8),
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20),
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(15, 8, 15, 8),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 20),
 
-              TextFormField(
+            TextFormField(
                 controller: emailController,
                 keyboardType: TextInputType.emailAddress,
                 decoration: InputDecoration(
-                    labelText: "Email Address",
-                    labelStyle: TextStyle(fontStyle: FontStyle.italic),
+                  labelText: "Email Address",
+                  labelStyle: TextStyle(fontStyle: FontStyle.italic),
                 ),
                 style: TextStyle(fontSize: 20),
                 validator: (value) {
@@ -43,79 +37,182 @@ class Login extends StatelessWidget {
                   }
                   return 'Please enter a valid email address';
                 }
-              ),
-              TextFormField(
+            ),
+            TextFormField(
                 controller: passwordController,
-                  keyboardType: TextInputType.text,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: "Password",
-                    labelStyle: TextStyle(fontStyle: FontStyle.italic),
-                  ),
-                  style: TextStyle(fontSize: 20),
-                  validator: (value) {
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  labelStyle: TextStyle(fontStyle: FontStyle.italic),
+                ),
+                style: TextStyle(fontSize: 20),
+                validator: (value) {
 
-                    bool numFound =  RegExp(r".*[0-9].*").hasMatch(value.toString());
-                    bool letterFound =  RegExp(r".*[A-Za-z].*").hasMatch(value.toString());
-                    bool spaceFound = RegExp(r".*[ ].*").hasMatch(value.toString());
-                    bool specCharFound = RegExp(r".*[\!\~\`\@\#\$\%\^\&\*\(\-\_\+\=\:\;\,\<\.\>\/\?].*").hasMatch(value.toString());
-                    if ( numFound == false || letterFound == false || specCharFound == false ) {
-                      return "Password must contain at least one number, one letter, and one special character";
-                    }
-                    if (spaceFound) {
-                      return "Password cannot have a space";
-                    }
-                    int v = value!.length;
-                    if ( v < 10 ){
-                      return "password should be at least 10 characters";
-                    }
-                    return null;
+                  bool numFound =  RegExp(r".*[0-9].*").hasMatch(value.toString());
+                  bool letterFound =  RegExp(r".*[A-Za-z].*").hasMatch(value.toString());
+                  bool spaceFound = RegExp(r".*[ ].*").hasMatch(value.toString());
+                  bool specCharFound = RegExp(r".*[\!\~\`\@\#\$\%\^\&\*\(\-\_\+\=\:\;\,\<\.\>\/\?].*").hasMatch(value.toString());
+                  if ( numFound == false || letterFound == false || specCharFound == false ) {
+                    return "Password must contain at least one number, one letter, and one special character";
                   }
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  print("pressed");
-                  // Validate returns true if the form is valid, or false otherwise.
-                  if (_formKey.currentState!.validate()){
-                    print("valid");
-                  }else{
-                    print("Not Valid");
+                  if (spaceFound) {
+                    return "Password cannot have a space";
                   }
+                  int v = value!.length;
+                  if ( v < 10 ){
+                    return "password should be at least 10 characters";
+                  }
+                  return null;
+                }
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print("pressed");
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState!.validate()){
+                  print("valid");
+                }else{
+                  print("Not Valid");
+                }
 
-                  if (_formKey.currentState!.validate()) {
-                    // If the form is valid, display a snackbar. In the real world,
-                    // you'd often call a server or save the information in a database.
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("Creating User")),
-                    );
-                    // try{
-                    //   FirebaseAuth.instance.createUserWithEmailAndPassword(
-                    //       email: emailController.text.trim(),
-                    //       password: passwordController.text.trim()).
-                    //   then((value) {
-                    //     ScaffoldMessenger.of(context).showSnackBar(
-                    //       const SnackBar(content: Text("Created")),
-                    //     );
-                    //     print(value);
-                    //     print(value.user);
-                    //     print(value.credential);
-                    //   });
-                    // }catch(err){
-                    //   sendVerificationEmail();
-                    //   print(err);
-                    //
-                    // }
-                  }
-                },
-                child: const Text('Login'),
-              ),
-              ElevatedButton(onPressed: (){}, child: Text("Register")),
-            ],
-          ),
+                if (_formKey.currentState!.validate()) {
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text("Creating User")),
+                  );
+                  // try{
+                  //   FirebaseAuth.instance.createUserWithEmailAndPassword(
+                  //       email: emailController.text.trim(),
+                  //       password: passwordController.text.trim()).
+                  //   then((value) {
+                  //     ScaffoldMessenger.of(context).showSnackBar(
+                  //       const SnackBar(content: Text("Created")),
+                  //     );
+                  //     print(value);
+                  //     print(value.user);
+                  //     print(value.credential);
+                  //   });
+                  // }catch(err){
+                  //   sendVerificationEmail();
+                  //   print(err);
+                  //
+                  // }
+                }
+              },
+              child: const Text('Login'),
+            ),
+            ElevatedButton(onPressed: (){
+              print("register Pressed");
+              registerCallBack();
+            }, child: Text("Register")),
+          ],
         ),
-
       ),
 
     );
+    // return Scaffold(
+    //   backgroundColor: Theme.of(context).backgroundColor,
+    //   // drawer: NavDrawer(),
+    //   appBar: AppBar(
+    //     title: Text("Log in/Register"),
+    //   ),
+    //   body: Form(
+    //     key: _formKey,
+    //     child: Padding(
+    //       padding: const EdgeInsets.fromLTRB(15, 8, 15, 8),
+    //       child: Column(
+    //         children: <Widget>[
+    //           SizedBox(height: 20),
+    //
+    //           TextFormField(
+    //             controller: emailController,
+    //             keyboardType: TextInputType.emailAddress,
+    //             decoration: InputDecoration(
+    //                 labelText: "Email Address",
+    //                 labelStyle: TextStyle(fontStyle: FontStyle.italic),
+    //             ),
+    //             style: TextStyle(fontSize: 20),
+    //             validator: (value) {
+    //               if ( RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value.toString()) ) {
+    //                 return null;
+    //               }
+    //               return 'Please enter a valid email address';
+    //             }
+    //           ),
+    //           TextFormField(
+    //             controller: passwordController,
+    //               keyboardType: TextInputType.text,
+    //               obscureText: true,
+    //               decoration: InputDecoration(
+    //                 labelText: "Password",
+    //                 labelStyle: TextStyle(fontStyle: FontStyle.italic),
+    //               ),
+    //               style: TextStyle(fontSize: 20),
+    //               validator: (value) {
+    //
+    //                 bool numFound =  RegExp(r".*[0-9].*").hasMatch(value.toString());
+    //                 bool letterFound =  RegExp(r".*[A-Za-z].*").hasMatch(value.toString());
+    //                 bool spaceFound = RegExp(r".*[ ].*").hasMatch(value.toString());
+    //                 bool specCharFound = RegExp(r".*[\!\~\`\@\#\$\%\^\&\*\(\-\_\+\=\:\;\,\<\.\>\/\?].*").hasMatch(value.toString());
+    //                 if ( numFound == false || letterFound == false || specCharFound == false ) {
+    //                   return "Password must contain at least one number, one letter, and one special character";
+    //                 }
+    //                 if (spaceFound) {
+    //                   return "Password cannot have a space";
+    //                 }
+    //                 int v = value!.length;
+    //                 if ( v < 10 ){
+    //                   return "password should be at least 10 characters";
+    //                 }
+    //                 return null;
+    //               }
+    //           ),
+    //           ElevatedButton(
+    //             onPressed: () {
+    //               print("pressed");
+    //               // Validate returns true if the form is valid, or false otherwise.
+    //               if (_formKey.currentState!.validate()){
+    //                 print("valid");
+    //               }else{
+    //                 print("Not Valid");
+    //               }
+    //
+    //               if (_formKey.currentState!.validate()) {
+    //                 // If the form is valid, display a snackbar. In the real world,
+    //                 // you'd often call a server or save the information in a database.
+    //                 ScaffoldMessenger.of(context).showSnackBar(
+    //                   const SnackBar(content: Text("Creating User")),
+    //                 );
+    //                 // try{
+    //                 //   FirebaseAuth.instance.createUserWithEmailAndPassword(
+    //                 //       email: emailController.text.trim(),
+    //                 //       password: passwordController.text.trim()).
+    //                 //   then((value) {
+    //                 //     ScaffoldMessenger.of(context).showSnackBar(
+    //                 //       const SnackBar(content: Text("Created")),
+    //                 //     );
+    //                 //     print(value);
+    //                 //     print(value.user);
+    //                 //     print(value.credential);
+    //                 //   });
+    //                 // }catch(err){
+    //                 //   sendVerificationEmail();
+    //                 //   print(err);
+    //                 //
+    //                 // }
+    //               }
+    //             },
+    //             child: const Text('Login'),
+    //           ),
+    //           ElevatedButton(onPressed: (){}, child: Text("Register")),
+    //         ],
+    //       ),
+    //     ),
+    //
+    //   ),
+    //
+    // );
   }
 }
